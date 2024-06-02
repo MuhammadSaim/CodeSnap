de270aee782a143fc0a51b0370dce8e0364c0874
Muhammad Saim <muhammadsaim494@gmail.com>
2024-06-03 00:59:20 +0500

    fix: rename JSON keys and order the JSON

--- backend/application/schemas/snap_schema.py
+++ backend/application/schemas/snap_schema.py
@@ -11,12 +11,15 @@ class SnapSchema(SQLAlchemySchema):
     # define meta class
     class Meta:
         model = Snap
+        ordered = True
+        fields = ('unique_code', 'image_base64', 'language', 'theme', 'created_at')
     
     # define fields
-    unique_code     = marshmallow.auto_field()
-    image_base64    = marshmallow.auto_field()
-    language        = fields.Nested(LanguageSchema)
-    theme           = fields.Nested(ThemeSchema)
+    unique_code     = fields.Str(attribute='unique_code', data_key='id')
+    image_base64    = fields.Str(attribute='image_base64', data_key='snap')
+    language        = fields.Nested(LanguageSchema, only=('name',))
+    theme           = fields.Nested(ThemeSchema, only=('name',))
+    created_at      = fields.DateTime(attribute='created_at', data_key='date_time')
     
 
 # define for single dump
