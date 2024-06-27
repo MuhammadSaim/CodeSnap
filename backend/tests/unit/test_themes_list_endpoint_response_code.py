cbf99d64766fdf4bc1a5667d86e6625efc85892d
Muhammad Saim <muhammadsaim494@gmail.com>
2024-06-27 15:42:58 +0500

    write test to check response length

--- backend/tests/unit/test_themes_list_endpoint_response_code.py
+++ backend/tests/unit/test_themes_list_endpoint_response_code.py
@@ -1,5 +1,9 @@
+from pygments.styles import get_all_styles
+
 def test_themes_list_endpoint_response_code(test_client, init_database):
     response = test_client.get('/api/v1/themes')
-    print(response.headers)
+    response_data = response.get_json()
+    themes = list(get_all_styles())
+    assert len(response_data) == len(themes) 
     assert response.status_code == 200
     
